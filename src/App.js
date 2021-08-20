0d9100bac695d3b17ae55223456a6ba8d95f6ca9
mo-hunter123 <ahrrass.00@gmail.com>
2021-08-21 00:00:29 +0100

    Another test

--- src/App.js
+++ src/App.js
@@ -6,17 +6,24 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          FOR SALE
+        </h1>
+        <h2>Buy this premium domain</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          contact &nbsp;
+          <a
+            className="App-link"
+            href="mailto:ahrrass.00@gmail.com?subject=Domain sale"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            ahrrass.00@gmail.com
+          </a>
+          &nbsp;
+          for more information
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
